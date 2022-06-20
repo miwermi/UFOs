ac65bef67f0bc23d1e1343701ff5fd2da4000c56
Michelle Werner <mdwerner@iupui.edu>
2022-06-20 04:13:38 -0400

    Uploading FINAL challenge files

--- static/js/app.js
+++ static/js/app.js
@@ -33,21 +33,25 @@ function updateFilters() {
 
     // 4a. Save the element that was changed as a variable.
     let srchInput = d3.select(this);
+    console.log(srchInput);
 
     // 4b. Save the value that was changed as a variable.
     let srchValue = srchInput.property("value");
+    console.log(srchValue);
 
     // 4c. Save the id of the filter that was changed as a variable.
     let filterID = srchInput.attr("id");
-  
+    console.log(filterID);
+
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
-  if (srchValue) {
-    srchFilters[filterID] = srchValue;
+    if (srchValue) {
+        srchFilters[filterID] = srchValue;
     }
     else {
-      delete srchFilters[filterID];
+        delete srchFilters[filterID];
     }
+    console.log(srchFilters);
   
     // 6. Call function to apply all filters and rebuild the table
     filterTable();
@@ -62,17 +66,16 @@ function updateFilters() {
   
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
-    for (var keys in srchFilters){
-    filteredData = filteredData.filter(row => row[keys] === srchFilters[keys]);
-  }  
+    Object.entries(srchFilters).forEach(([key, value]) => {
+        filteredData = filteredData.filter(row => row[key] === value);
+    });  
   
     // 10. Finally, rebuild the table using the filtered data
     buildTable(filteredData);
-  
   }
   
   // 2. Attach an event to listen for changes to each filter
-  d3.selectAll("#filter-btn").on("click", handleClick);
+  d3.selectAll("input").on("change", updateFilters);
   
   // Build the table when the page loads
   buildTable(tableData);
